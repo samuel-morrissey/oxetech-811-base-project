315832d6c0578f704718094513b9ee3ae4ecbb2e
thiagoegr <tegr@ic.ufal.br>
2026-06-28 19:34:13 -0300

    refactor: extract Express app configuration

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import cors from "cors";
+import express from "express";
+import "dotenv/config";
+import router from "./routes/routes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", router);
+
+app.use((_request, response) => {
+    response.status(404).json({ message: "Rota nao encontrada" });
+});
+
+export default app;
