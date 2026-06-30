8bbb38f0b9378dca45ae08f5734b8ba4ef4b27f7
Frederico Santos Saggio <saggiofrederico@gmail.com>
2026-06-30 00:43:18 -0300

    feat: add test coverage configuration and npm script

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,11 @@ module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
 	testMatch: ["<rootDir>/tests/**/*.test.ts"],
+	collectCoverageFrom: [
+		"src/**/*.ts",
+		"!src/server.ts",
+		"!src/seed.ts",
+	],
+	coverageDirectory: "coverage",
 	clearMocks: true,
 };
