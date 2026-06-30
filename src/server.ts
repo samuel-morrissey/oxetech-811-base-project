db484df4e514f17cff5495d328a06d57e5e26113
Frederico Santos Saggio <saggiofrederico@gmail.com>
2026-06-30 00:03:49 -0300

    feat: implement  express JSON body limit to 100kb

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 const port = Number(process.env.PORT || 3000);
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "100kb" }));
 app.use("/api", router);
 
 app.use((_request, response) => {
